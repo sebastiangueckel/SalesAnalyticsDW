c25d9a231df9d01d53833f64ab561bb2f4e504c9
sebastiangueckel <sebastian.gueckel00@gmail.com>
2026-07-24 16:52:21 +0200

    Dimensionstabellen in PostgreSQL Datenbank geladen.

--- ETL/dim_loader.py
+++ ETL/dim_loader.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from ETL.date_dim_create import date_dim
+from config.config import engine
+from sqlalchemy import text
 
 customers = pd.read_csv("../data/raw/customers.csv")
 products = pd.read_csv("../data/raw/products.csv")
@@ -50,5 +52,91 @@ orders = orders.merge(
     how="left"
 )
 orders = orders.drop(columns=["date"])
+customers = customers.drop(columns=["region_key"])
 
-print(orders.columns)
+# Einheitliche Spaltennamen
+orders.columns = orders.columns.str.lower()
+customers.columns = customers.columns.str.lower()
+date_dim.columns = date_dim.columns.str.lower()
+employees.columns = employees.columns.str.lower()
+products.columns = products.columns.str.lower()
+regions.columns = regions.columns.str.lower()
+
+
+# Dimensionsdaten einlesen
+with engine.begin() as conn:
+    conn.execute(text("TRUNCATE TABLE customers_dim CASCADE"))
+
+customers.to_sql(
+    "customers_dim",
+    engine,
+    if_exists="append",
+    index=False
+)
+
+print("✔ customers_dim erfolgreich befüllt.")
+
+# Dimensionsdaten einlesen
+with engine.begin() as conn:
+    conn.execute(text("TRUNCATE TABLE date_dim CASCADE"))
+
+date_dim.to_sql(
+    "date_dim",
+    engine,
+    if_exists="append",
+    index=False
+)
+
+print("✔ date_dim erfolgreich befüllt.")
+
+# Dimensionsdaten einlesen
+with engine.begin() as conn:
+    conn.execute(text("TRUNCATE TABLE employees_dim CASCADE"))
+
+employees.to_sql(
+    "employees_dim",
+    engine,
+    if_exists="append",
+    index=False
+)
+
+print("✔ employees_dim erfolgreich befüllt.")
+
+# Dimensionsdaten einlesen
+with engine.begin() as conn:
+    conn.execute(text("TRUNCATE TABLE products_dim CASCADE"))
+
+products.to_sql(
+    "products_dim",
+    engine,
+    if_exists="append",
+    index=False
+)
+
+print("✔ products_dim erfolgreich befüllt.")
+
+# Dimensionsdaten einlesen
+with engine.begin() as conn:
+    conn.execute(text("TRUNCATE TABLE regions_dim CASCADE"))
+
+regions.to_sql(
+    "regions_dim",
+    engine,
+    if_exists="append",
+    index=False
+)
+
+print("✔ regions_dim erfolgreich befüllt.")
+
+# Dimensionsdaten einlesen
+with engine.begin() as conn:
+    conn.execute(text("TRUNCATE TABLE orders_dim CASCADE"))
+
+orders.to_sql(
+    "orders_dim",
+    engine,
+    if_exists="append",
+    index=False
+)
+
+print("✔ orders_dim erfolgreich befüllt.")
